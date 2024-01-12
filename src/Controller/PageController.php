f2f8964ec62681745bfceed448133afe6924b07d
Jensone <hello@jensone.com>
2024-01-12 19:18:51 +0100

    🐛 corriger(PageController.php) : corriger les chemins de rendu des templates pour correspondre aux noms de pages

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,31 +11,31 @@ class PageController extends AbstractController
     #[Route('/', name: 'paris')]
     public function paris(): Response
     {
-        return $this->render('homepage/paris.html.twig', [
+        return $this->render('page/paris.html.twig', [
         ]);
     }
     #[Route('/las-vegas', name: 'lasvegas')]
     public function lasvegas(): Response
     {
-        return $this->render('homepage/paris.html.twig', [
+        return $this->render('page/paris.html.twig', [
         ]);
     }
     #[Route('/kyoto', name: 'kyoto')]
     public function kyoto(): Response
     {
-        return $this->render('homepage/paris.html.twig', [
+        return $this->render('page/paris.html.twig', [
         ]);
     }
     #[Route('/sydney', name: 'sydney')]
     public function sydney(): Response
     {
-        return $this->render('homepage/paris.html.twig', [
+        return $this->render('page/paris.html.twig', [
         ]);
     }
     #[Route('/hong-kong', name: 'hongkong')]
     public function hongkong(): Response
     {
-        return $this->render('homepage/paris.html.twig', [
+        return $this->render('page/paris.html.twig', [
         ]);
     }
 }
