6f9a4aec37648a2289e1333eca7f8128e3a004c7
Jensone <hello@jensone.com>
2024-01-12 19:56:30 +0100

    🔧 corriger(.gitignore) : ajouter les fichiers .DS_Store à l'ignorer 🎨 style(custom.css) : ajouter un style de survol pour le bouton .btn-custom 📝 ajouter(hongkong.css) : ajouter un fichier de style pour la ville de Hong Kong 📝 ajouter(kyoto.css) : ajouter un fichier de style pour la ville de Kyoto 📝 ajouter(lasvegas.css) : ajouter un fichier de style pour la ville de Las Vegas 📝 ajouter(sydney.css) : ajouter un fichier de style pour la ville de Sydney 🐛 corriger(PageController.php) : corriger les noms de fichiers de template et ajouter les variables de titre, sous-titre et arrière-plan 🐛 corriger(base.html.twig) : corriger les liens vers les pages des villes en utilisant les routes Symfony 📝 ajouter(city.html.twig) : ajouter un template pour les pages des villes avec les variables de titre, sous-titre et arrière-plan

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,31 +11,50 @@ class PageController extends AbstractController
     #[Route('/', name: 'paris')]
     public function paris(): Response
     {
-        return $this->render('page/paris.html.twig', [
+        return $this->render('page/city.html.twig', [
+            'title' => 'paris',
+            'subtitle' => 'paris',
+            'background' => 'css/paris.css'
         ]);
     }
     #[Route('/las-vegas', name: 'lasvegas')]
     public function lasvegas(): Response
     {
-        return $this->render('page/paris.html.twig', [
+        return $this->render('page/city.html.twig', [
+            'title' => 'las vegas',
+            'subtitle' => 'las vegas',
+            'background' => 'css/lasvegas.css'
+
         ]);
     }
     #[Route('/kyoto', name: 'kyoto')]
     public function kyoto(): Response
     {
-        return $this->render('page/paris.html.twig', [
+        return $this->render('page/city.html.twig', [
+            'title' => 'kyoto',
+            'subtitle' => '京都市',
+            'background' => 'css/kyoto.css'
+
         ]);
     }
     #[Route('/sydney', name: 'sydney')]
     public function sydney(): Response
     {
-        return $this->render('page/paris.html.twig', [
+        return $this->render('page/city.html.twig', [
+            'title' => 'sydney',
+            'subtitle' => 'sydney',
+            'background' => 'css/sydney.css'
+
         ]);
     }
     #[Route('/hong-kong', name: 'hongkong')]
     public function hongkong(): Response
     {
-        return $this->render('page/paris.html.twig', [
+        return $this->render('page/city.html.twig', [
+            'title' => 'hong kong',
+            'subtitle' => '香港',
+            'background' => 'css/hongkong.css'
+
         ]);
     }
 }
