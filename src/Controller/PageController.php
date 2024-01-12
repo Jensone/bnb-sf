fb91f090947c33a69aca38305c0cf3d68fae1b65
Jensone <hello@jensone.com>
2024-01-12 19:18:04 +0100

    🎨 style(custom.css): ajuster la taille de la police pour utiliser des unités de vue (vh) au lieu de rem 🔥 suppression(HomepageController.php): supprimer le contrôleur HomepageController ✨ fonctionnalité(PageController.php): ajouter le contrôleur PageController pour gérer les différentes pages 🎉 ajout(paris.html.twig): ajouter le template paris.html.twig pour la page Paris 🔧 corriger(register.html.twig): déplacer le titre "Créer un compte" à l'intérieur du formulaire 🔧 corriger(login.html.twig): déplacer le titre "Connexion" à l'intérieur du formulaire

---
+++ src/Controller/PageController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'paris')]
+    public function paris(): Response
+    {
+        return $this->render('homepage/paris.html.twig', [
+        ]);
+    }
+    #[Route('/las-vegas', name: 'lasvegas')]
+    public function lasvegas(): Response
+    {
+        return $this->render('homepage/paris.html.twig', [
+        ]);
+    }
+    #[Route('/kyoto', name: 'kyoto')]
+    public function kyoto(): Response
+    {
+        return $this->render('homepage/paris.html.twig', [
+        ]);
+    }
+    #[Route('/sydney', name: 'sydney')]
+    public function sydney(): Response
+    {
+        return $this->render('homepage/paris.html.twig', [
+        ]);
+    }
+    #[Route('/hong-kong', name: 'hongkong')]
+    public function hongkong(): Response
+    {
+        return $this->render('homepage/paris.html.twig', [
+        ]);
+    }
+}
