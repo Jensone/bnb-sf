d86459b5222061008a1e4f5a988508d3f1ef4325
Jensone <hello@jensone.com>
2024-01-14 20:52:56 +0100

    🔧 corriger(AppFixtures.php) : supprimer l'image par défaut pour l'administrateur ✨ fonctionnalité(AppFixtures.php) : ajouter des équipements aux chambres ✨ fonctionnalité(AppFixtures.php) : ajouter des favoris pour l'administrateur ✨ fonctionnalité(AppFixtures.php) : ajouter des utilisateurs avec des favoris

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,16 +15,16 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
-
+        // Set admin
         $admin = new User();
         $admin->setEmail('admin@admin.fr')
             ->setRoles(['ROLE_ADMIN'])
-            ->setImage('https://placehold.it/64x64')
             ->setPassword('$2y$13$wqXiXE8U6QhYtIRJFedLA.MkNVmDzn89jVz5CBYENUOwHfAlyYNG2');
         $manager->persist($admin);
 
+        // Set hosts
         $hosts = [];
-        for ($i=0; $i < 4; $i++) { 
+        for ($i=0; $i < 8; $i++) { 
             $host = new User();
             $host->setEmail('host' . $i . '@host.fr')
                 ->setRoles(['ROLE_HOST'])
@@ -35,6 +35,7 @@ class AppFixtures extends Fixture
             array_push($hosts, $host);
         }
 
+        // Set equipments
         $equipments = ['wifi', 'tv', 'climatiseur', 'lave-linge', 'lave-vaisselle', 'piscine', 'jacuzzi', 'parking'];
         $equipmentArray = [];
         for ($i=0; $i < count($equipments); $i++) { 
@@ -44,8 +45,10 @@ class AppFixtures extends Fixture
             array_push($equipmentArray, $equipment);
         }
 
+        // Set rooms
         $cities = ['paris', 'las-vegas', 'kyoto', 'sydney', 'hong-kong'];
         for ($i = 0; $i < 100; $i++) {
+            
             $room = new Room();
             $room->setName($faker->words(3, true))
                 ->setSlug($faker->slug)
@@ -53,11 +56,31 @@ class AppFixtures extends Fixture
                 ->addEquipment($faker->randomElement($equipmentArray))
                 ->addEquipment($faker->randomElement($equipmentArray))
                 ->setDescription($faker->paragraphs(3, true))
-                ->setHost($faker->randomElement($hosts));
+                ->setHost($faker->randomElement($hosts))
+                ->setPrice($faker->numberBetween(150, 1500))
+                ->setCreatedAt(new \DateTime('now'))
+                ->setUpdatedAt(new \DateTime('now'));
 
+            // Add favorites to admin
+            if ($i < 10) {
+                $admin->addFavorite($room);
+            }
+
+            // Set users with favorites
+            if ($i > 70) {
+                    $user = new User();
+                    $user->setEmail('user' . $i . '@user.fr')
+                        ->setRoles(['ROLE_USER'])
+                        ->setFirstname($faker->firstName)
+                        ->setLastname($faker->lastName)
+                        ->setPassword('$2y$13$wqXiXE8U6QhYtIRJFedLA.MkNVmDzn89jVz5CBYENUOwHfAlyYNG2')
+                        ->addFavorite($room);
+                    $manager->persist($user);
+            }
             $manager->persist($room);
         }
 
+        // Flush
         $manager->flush();
     }
 }
